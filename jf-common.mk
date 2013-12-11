faec860a47840c775b599016425c2b5dcf031317
cphelps76 <cphelps76@gmail.com>
2013-12-10 21:22:41 -0600

    Turbo S4...Gummy with a 150 shot of nitrous and 18 lbs of boost...
    go fast or go home
    
    Change-Id: I219aa1f7e01c014a3763b4508465f4bb482d95c9

--- jf-common.mk
+++ jf-common.mk
@@ -189,7 +189,44 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
-    ro.telephony.ril.v3=newDriverCall
+    ro.telephony.ril.v3=newDriverCall \
+    windowsmgr.max_events_per_sec=500 \
+    ro.kernel.android.checkjni=0 \
+    ro.media.enc.jpeg.quality=100 \
+    ro.config.nocheckin=1 \
+    debug.performance.tuning=1 \
+    persist.service.swiqi.enable=1 \
+    video.accelerate.hw=1 \
+    ro.ril.hep=1 \
+    ro.ril.hsxpa=3 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.enable.a53=1 \
+    ro.ril.hsupa.category=5 \
+    persist.sys.purgeable_assets=1 \
+    ro.home_app_adj=1 \
+    ro.ext4fs=1 \
+    persist.sys.use_dithering=0 \
+    ro.ril.disable.power.collapse=0 \
+    dalvik.vm.heapstartsize=128m \
+    dalvik.vm.heapgrowthlimit=320m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.heaputilization=0.05 \
+    dalvik.vm.heapidealfree=8388608 \
+    dalvik.vm.heapconcurrentstart=2097152 \
+    dalvik.vm.verify-bytecode=false \
+    dalvik.vm.dexopt-data-only=1 \
+    ro.config.hw_quickpoweron=true \
+    ro.dalvik.vm.checkjni=0 \
+    dalvik.vm.dexopt-flags=v=n,o=v \
+    debug.composition.type=gpu \
+    ro.max.fling_velocity=15000 \
+    ro.min.fling_velocity=8000 \
+    ro.config.hw_fast_dormancy=1 \
+    net.dns1=8.8.4.4 \
+    net.dns2=8.8.8.8
 
 # call common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
